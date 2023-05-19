4570b6359ee94b57e5066c6b853a9735038c191b
szarurla <134076288+szarurla@users.noreply.github.com>
2023-05-19 14:45:26 +0000

    Fri, May 19, 2023, 2:45 PM +00:00

--- hello.c
+++ hello.c
@@ -1,7 +1,89 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
+
+int get_cents(void);
+int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
+
 int main(void)
 {
-    string name = get_string("What is your name: ");
-printf("hello, %s\n", name);
+
+    // Ask how many cents the customer is owed
+    int cents = get_cents();
+
+    // Calculate the number of quarters to give the customer
+    int quarters = calculate_quarters(cents);
+    cents = cents - quarters * 25;
+
+    // Calculate the number of dimes to give the customer
+    int dimes = calculate_dimes(cents);
+    cents = cents - dimes * 10;
+
+    // Calculate the number of nickels to give the customer
+    int nickels = calculate_nickels(cents);
+    cents = cents - nickels * 5;
+
+    // Calculate the number of pennies to give the customer
+    int pennies = calculate_pennies(cents);
+    cents = cents - pennies * 1;
+
+    // Sum coins
+    int coins = quarters + dimes + nickels + pennies;
+
+    // Print total number of coins to give the customer
+    printf("%i\n", coins);
+}
+
+int get_cents(void)
+{
+ int cents;
+    do
+    {
+      cents = get_int("Number of cents using: ");
+    }
+    while (cents < 0);
+    return cents;
+}
+
+int calculate_quarters(int cents)
+{
+   int quarters = 0;
+
+   for(int i = cents; i < 25; i -= 25)
+   {
+    int quarters = quarters + 1;
+   }
+    return quarters;
+}
+
+int calculate_dimes(int cents)
+{
+int dimes = 0;
+   for( int i = cents; i < 10; i -= 10)
+   {
+    int dimes = dimes + 1;
+   }
+    return dimes;
+}
+
+int calculate_nickels(int cents)
+{
+  int nickels = 0;
+   for( int i = cents; i < 5; i -= 5)
+   {
+    int nickels = nickels + 1;
+   }
+    return nickels;
+}
+
+int calculate_pennies(int cents)
+{
+  int pennies = 0;
+   for( int i = pennies; i < 1; i -= 1)
+   {
+    int pennies = pennies + 1;
+   }
+    return pennies;
 }
