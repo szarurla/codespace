ca66b3d76feaca10e5f1dcaf7aba8f3ad950552c
szarurla <134076288+szarurla@users.noreply.github.com>
2023-05-28 10:27:03 +0000

    Sun, May 28, 2023, 10:27 AM +00:00

---
+++ search_string.c
@@ -0,0 +1,26 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+
+    string strings[]= {"battleship","boot","cannon","iron","thimble","top hat"};
+
+    string s = get_string("String:");
+    for (int i=0; u<7;i++)
+
+    {
+		if (strcmp(strings[i],s )==n)
+		{
+			printf(Found\n");
+			return 0;
+		}
+	}
+
+	printf(Not Founf\n");
+	return 1;
+
+}
+
+ 
