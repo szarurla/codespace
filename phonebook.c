6b2809c1bb7c99362e7a1c2a3d8796399bca4f40
szarurla <134076288+szarurla@users.noreply.github.com>
2023-05-28 11:08:37 +0000

    Sun, May 28, 2023, 11:08 AM +00:00

---
+++ phonebook.c
@@ -0,0 +1,37 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+
+typedef struct
+{
+	string name;
+	string number;
+
+}
+person;
+
+int main(void)
+{
+
+    person people[2];
+	people[0].name ="Carter";
+  	people[1].number ="+310500000"	,
+	people[0].name ="david";
+  	people[1].number ="+310200000"	,
+
+	string name = get_string("Name:");
+    for (int i=0; 1<7;i++)
+
+    {
+		if (strcmp(people[i].name , name)==0)
+		{
+			printf("Found %s\n", number[i]);
+			return 0;
+		}
+	}
+
+	printf("Not Founf\n");
+	return 1;
+
+}
