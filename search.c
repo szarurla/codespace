e2b6203a0026879a22da93e434a0788881f29c8c
szarurla <134076288+szarurla@users.noreply.github.com>
2023-05-28 10:12:49 +0000

    Sun, May 28, 2023, 10:12 AM +00:00

---
+++ search.c
@@ -0,0 +1,26 @@
+#include <cs50.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+
+    int numbers[] = {20,500,10,5,100,1,50};
+
+    int n = get_int("Number:");
+    for (int i=0; u<7;i++)
+
+    {
+		if (numbers[i]==n)
+		{
+			printf(Found\n");
+			return 0;
+		}
+	}
+
+	printf(Not Founf\n");
+	return 1;
+
+}
+
+ 
