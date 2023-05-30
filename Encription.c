237318a740d5a127cd31bb31fa642d98978e930d
szarurla <134076288+szarurla@users.noreply.github.com>
2023-05-30 19:46:43 +0000

    Tue, May 30, 2023, 7:46 PM +00:00

---
+++ Encription.c
@@ -0,0 +1,66 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+bool isallalpha(string str)
+{
+    int counts[26] = {0};
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (!isalpha(str[i]))
+        {
+            return false;
+        }
+        int index = tolower(str[i]) - 'a';
+        if (counts[index] > 0)
+        {
+            return false;
+        }
+        counts[index]++;
+    }
+
+    return true;
+}
+
+int main(int argc, string argv[])
+{
+    if (argc != 2 || strlen(argv[1]) != 26 || !isallalpha(argv[1]))
+    {
+        printf("Use 26 alphabetic characters as command-line arguments.\n");
+        return 1;
+    }
+
+    string word = get_string("Input a word to encrypt: ");
+    string ciphertextHigh = argv[1];
+    string ciphertextLow = "";
+    for (int j = 0; j < 26; j++)
+    {
+        ciphertextLow[j] = ciphertextHigh[j - 32];
+        printf("%c", ciphertextLow[j]);
+    }
+  printf("%s %s \n", ciphertextLow, ciphertextHigh);
+    printf("ciphertext: ");
+    for (int i = 0; i < strlen(word); i++)
+    {
+        int word_char_value = word[i];
+        if (isupper(word[i]))
+        {
+            if (word_char_value >= 65 && word_char_value <= 90)
+            {
+                printf("%c", ciphertextHigh[word_char_value - 65]);
+            }
+        }
+        else if (islower(word[i]))
+        {
+            if (word_char_value >= 97 && word_char_value <= 122)
+            {
+                printf("%c", ciphertextLow[word_char_value - 97]);
+            }
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}
