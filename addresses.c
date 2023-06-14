15ce70b09cb1222577d1a7acb51825179521ffc0
szarurla <134076288+szarurla@users.noreply.github.com>
2023-06-14 10:14:10 +0000

    Wed, Jun 14, 2023, 10:14 AM +00:00

--- addresses.c
+++ addresses.c
@@ -10,4 +10,9 @@ int main(void)
     printf("%c\n", *(s+1));
     printf("%c\n", *(s+2));
     printf("%c\n", *(s+3));
+    printf("%c\n", *(s+4));
+    for (int i=0; i<10;i++)
+    {
+      printf("%c", *(s+i));
+    }
 }
